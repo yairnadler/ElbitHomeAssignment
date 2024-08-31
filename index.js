310b16ed0d17d5daa8ea9b5d7885e8f163059d31
Yair Nadler <yair.nadler7@gmail.com>
2024-08-31 17:14:30 +0300

    routes

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import express, { json, urlencoded } from "express";
-import {
-  getAllFlights,
-  getDelayedFlights,
-  getFlightsByCountryName,
-  getInboundFlights,
-  getInboundFlightsByCountryName,
-  getMostPopularDestinationv,
-  getOutboundFlights,
-  getOutboundFlightsByCountryName,
-} from "./controllers/flights.controller.js";
+import router from "./routes/flights.routes.js";
 import { PORT } from "./utils/constants.js";
 import morgan from "morgan";
 
@@ -17,31 +8,8 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
-// app.use('/', router);
 
-// route that returns the amount of all flights
-app.get("/all", getAllFlights);
-
-// route that returns number of all inbound flights
-app.get("/inbound", getInboundFlights);
-
-// route that returns number of all outbound flights
-app.get("/outbound", getOutboundFlights);
-
-// route that returns number of flights by country's name
-app.get("/country", getFlightsByCountryName);
-
-// route that returns all inbound flights by country's name
-app.get("/inbound/country", getInboundFlightsByCountryName);
-
-// route that returns all outbound flights by country's name
-app.get("/outbound/country", getOutboundFlightsByCountryName);
-
-// route that returns all delayed flights
-app.get("/delayed", getDelayedFlights);
-
-// route that returns the most popular destination
-app.get("/mostPopular", getMostPopularDestinationv);
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
---
+++ routes/flights.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import {
+    getAllFlights,
+    getDelayedFlights,
+    getFlightsByCountryName,
+    getInboundFlights,
+    getInboundFlightsByCountryName,
+    getMostPopularDestinationv,
+    getOutboundFlights,
+    getOutboundFlightsByCountryName,
+  } from "../controllers/flights.controller.js";
+
+const router = Router();
+
+
+// route that returns the amount of all flights
+router.get("/all", getAllFlights);
+
+// route that returns number of all inbound flights
+router.get("/inbound", getInboundFlights);
+
+// route that returns number of all outbound flights
+router.get("/outbound", getOutboundFlights);
+
+// route that returns number of flights by country's name
+router.get("/country", getFlightsByCountryName);
+
+// route that returns all inbound flights by country's name
+router.get("/inbound/country", getInboundFlightsByCountryName);
+
+// route that returns all outbound flights by country's name
+router.get("/outbound/country", getOutboundFlightsByCountryName);
+
+// route that returns all delayed flights
+router.get("/delayed", getDelayedFlights);
+
+// route that returns the most popular destination
+router.get("/mostPopular", getMostPopularDestinationv)
+
+export default router;
